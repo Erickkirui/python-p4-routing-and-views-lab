491969b69355ede2371b2a73de3fc8e8a5763045
Erickkirui <erickkirui653@gmail.com>
2023-06-20 11:55:38 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<text>')
+def print_text(text):
+    return f'hello{text}'
+
+@app.route('/count/<int:num>')
+def count(num):
+    result = ''
+    for i in range(num):
+        result += str(i) + '\n'
+    return result
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
